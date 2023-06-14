ab938354f10281901d541c831e15aa2298406ae4
hemapriya <hema47173@gmail.com>
2023-06-14 18:14:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+
+import axios from "axios";
+
+import { Provider } from "react-redux";
+
+import App from "./App";
+import "./index.css";
+import { store } from "./store";
+axios.defaults.baseURL = "https://travel-diaries-server.herokuapp.com";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { isLoggedIn: false },
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = true;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export const store = configureStore({ reducer: authSlice.reducer });
